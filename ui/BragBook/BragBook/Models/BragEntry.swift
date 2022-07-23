8f043ae59e6d923e9c09a6163ad82450e79f5e12
Angela Mitchell <anglarenmit@gmail.com>
2022-07-23 17:45:06 -0500

    create pill component for tag

--- ui/BragBook/BragBook/Models/BragEntry.swift
+++ ui/BragBook/BragBook/Models/BragEntry.swift
@@ -11,9 +11,9 @@ struct BragEntry: Identifiable {
     let id: UUID
     var title: String
     var description: String
-    var tags: [String]
+    var tags: [BragTag]
     
-    init(id: UUID = UUID(), title: String, description: String, tags: [String]) {
+    init(id: UUID = UUID(), title: String, description: String, tags: [BragTag]) {
         self.id = id
         self.title = title
         self.description = description
@@ -24,8 +24,29 @@ struct BragEntry: Identifiable {
 extension BragEntry {
     static let sampleData: [BragEntry] =
     [
-        BragEntry(title: "Pastry Card Component", description: "I created a card view in SwiftUI for bakery shop's application.", tags: ["short-term", "front-end", "web design", "swift ui", "component"]),
-        BragEntry(title: "Navigation Bar Animation", description: "I implemented an animation for the navigation bar's cta's hover states.", tags: ["Web Design", "front-end"]),
-        BragEntry(title: "Search Component - AEM", description: "I build the foundational functionality for the search component for a telecommunications company", tags: ["AEM", "Docker"]),
+        BragEntry(
+            title: "Pastry Card Component",
+            description: "I created a card view in SwiftUI for bakery shop's application.",
+            tags: [
+                BragTag.sampleTags[0],
+                BragTag.sampleTags[1],
+                BragTag.sampleTags[2],
+                BragTag.sampleTags[3],
+                BragTag.sampleTags[4]
+            ]),
+        BragEntry(
+            title: "Navigation Bar Animation",
+            description: "I implemented an animation for the navigation bar's cta's hover states.",
+            tags: [
+                BragTag.sampleTags[2],
+                BragTag.sampleTags[1]
+            ]),
+        BragEntry(
+            title: "Search Component - AEM",
+            description: "I build the foundational functionality for the search component for a telecommunications company",
+            tags: [
+                BragTag.sampleTags[5],
+                BragTag.sampleTags[6]
+            ]),
     ]
 }
