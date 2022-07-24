156517aa83974fed987aa791a98b0a8379f645a2
EXW406 <elizabeth.wait@sherwin.com>
2022-07-24 01:55:51 -0500

    Created JourneyView

--- ui/BragBook/BragBook/JourneyView.swift
+++ ui/BragBook/BragBook/JourneyView.swift
@@ -9,7 +9,68 @@ import SwiftUI
 
 struct JourneyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "chevron.backward").foregroundColor(Color("Dark Green"))
+                .font(.system(size: 30, weight: .regular))
+                .padding(.leading, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            Text("Hi, Savannah")
+                .foregroundColor(Color("Dark Green"))
+                .font(.system(size: 20, weight: .semibold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.top, 30)
+            Text("Jump Back In")
+                .foregroundColor(Color("Green"))
+                .font(.system(size: 16, weight: .semibold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.top, -5)
+                .padding(.bottom, 15)
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                           .fill(.gray)
+                           .frame(width: 400, height: 150)
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                           .fill(.gray)
+                           .frame(width: 400, height: 150)
+            HStack {
+            Text("Popular Courses")
+                .foregroundColor(Color("Dark Green"))
+                .font(.system(size: 20, weight: .semibold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 30)
+                .padding(.leading, 20)
+                .padding(.bottom, 15)
+                
+                HStack {
+                Text("Show All")
+                    .foregroundColor(Color("Green"))
+                    .font(.system(size: 15, weight: .semibold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 30)
+                    .padding(.leading, 107)
+                    .padding(.bottom, 15)
+                    Image(systemName: "chevron.forward").foregroundColor(Color("Green"))
+                        .font(.system(size: 15, weight: .semibold))
+                        .padding(.top, 30)
+                        .padding(.trailing, 20)
+                        .padding(.bottom, 15)
+                }
+            }
+            ScrollView(.horizontal) {
+                HStack(spacing: 20) {
+                    ForEach(0..<10) {
+                        Text("Item \($0)")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .frame(width: 200, height: 200)
+                            .background(Color("Dark Green"))
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
