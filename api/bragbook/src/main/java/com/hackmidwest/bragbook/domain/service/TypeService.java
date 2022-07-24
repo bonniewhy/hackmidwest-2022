4132ee631a5a1cce417c2e6806f10b1b90baa215
Lynelle Camp <lynelle.camp@vmlyr.com>
2022-07-23 23:22:35 -0500

    Updates to type and add type exception

--- api/bragbook/src/main/java/com/hackmidwest/bragbook/domain/service/TypeService.java
+++ api/bragbook/src/main/java/com/hackmidwest/bragbook/domain/service/TypeService.java
@@ -2,9 +2,12 @@ package com.hackmidwest.bragbook.domain.service;
 
 import com.hackmidwest.bragbook.domain.entity.Type;
 
+import java.util.List;
+
 public interface TypeService {
     String saveType(Type type);
     Type getTypeById(String typeId);
+    List<Type> getAllTypes();
     Type updateType(String typeId, Type type);
     void deleteType(String typeId);
 }
